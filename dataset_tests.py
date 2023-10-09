f07ba3c175ea0a313541d8e7e2b338892b65b9b2
EauDeData <evabot505@gmail.com>
2023-10-09 15:07:27 +0000

    sin comerte entteeeeraaaa

--- dataset_tests.py
+++ dataset_tests.py
@@ -1,4 +1,4 @@
-from src.datasets.ocr.test.ocr_datasets_unitary_testing import try_esposalles, try_cocotext, try_funsd, try_washinton, try_hiertext, try_maps
+from src.datasets.ocr.test.ocr_datasets_unitary_testing import try_esposalles, try_cocotext, try_funsd, try_washinton, try_hiertext, try_maps, try_iam
 
 try_esposalles()
 try_cocotext()
@@ -6,3 +6,4 @@ try_funsd()
 try_washinton()
 try_hiertext()
 try_maps()
+try_iam()
--- src/datasets/ocr/test/ocr_datasets_unitary_testing.py
+++ src/datasets/ocr/test/ocr_datasets_unitary_testing.py
@@ -6,47 +6,41 @@ from src.datasets.ocr.funsd import FUNSDDataset, DEFAULT_FUNSD
 from src.datasets.ocr.washington import GWDataset, DEFAULT_WASHINGTON
 from src.datasets.ocr.hiertext import HierTextDataset, DEFAULT_HIERTEXT
 from src.datasets.ocr.historical_maps import HistoricalMapsdDataset, DEFAULT_HIST_MAPS
+from src.datasets.ocr.iam import IAMDataset, DEFAULT_IAM
+
 
 IDX = 42
 OUTPUT_TMP_FOLDER = './tmp_/'
 os.makedirs(OUTPUT_TMP_FOLDER, exist_ok=True)
 
+def log_dataset(dataset, idx = IDX, image_to_observe = 'original_image'):
+    print('total:', len(dataset))
+    out = dataset[IDX]
+    print(out)
+    out[image_to_observe].save(os.path.join(OUTPUT_TMP_FOLDER, f"{out['dataset']}_{out['split']}.png"))
+
 def try_esposalles(base_folder = DEFAULT_ESPOSALLES, split = 'train', cross_val = 'cv1', mode = 'words', image_height = 128, patch_width = 16, transforms = lambda x: x):
 
     dataset = EsposalledDataset(base_folder, split, cross_val, mode, image_height, patch_width, transforms)
-    print('total:', len(dataset))
-
-    
-    out = dataset[IDX]
-    print(out)
-    out['original_image'].save(os.path.join(OUTPUT_TMP_FOLDER, f"{out['dataset']}_{out['split']}.png"))
-    
+    log_dataset(dataset)
     
 def try_cocotext(base_folder = DEFAULT_COCOTEXT, annots_name='cocotext.v2.json', langs = ['english', 'non-english'], legibility = ['legible', 'illgible'], split = 'train', image_height = 128, patch_width = 16, transforms = lambda x: x):
     dataset = COCOTextDataset(base_folder, annots_name, split, langs, legibility, image_height, patch_width, transforms )
-    out = dataset[IDX]
-    print(out)
-    out['original_image'].save(os.path.join(OUTPUT_TMP_FOLDER, f"{out['dataset']}_{out['split']}.png"))
-    
-    print('total:', len(dataset))
+    log_dataset(dataset)
+
 
     
 def try_funsd(base_folder = DEFAULT_FUNSD, split: ['train', 'test'] = 'train', patch_width = 16, image_height = 128, transformations = lambda x: x):
     
     dataset = FUNSDDataset(base_folder, split, patch_width, image_height, transformations)
-    print('total:', len(dataset))
-    out = dataset[IDX]
-    print(out)
-    out['original_image'].save(os.path.join(OUTPUT_TMP_FOLDER, f"{out['dataset']}_{out['split']}.png"))
+    log_dataset(dataset)
+
     
 def try_washinton(base_folder = DEFAULT_WASHINGTON, split: ['train', 'test', 'val'] = 'train', cross_val = 'cv1', mode = 'word', image_height = 128, patch_width = 16, transforms = lambda x: x):
     
     dataset = GWDataset(base_folder, split, cross_val, mode, image_height, patch_width, transforms)
-    
-    print('total:', len(dataset))
-    out = dataset[IDX]
-    print(out)
-    out['original_image'].save(os.path.join(OUTPUT_TMP_FOLDER, f"{out['dataset']}_{out['split']}.png"))
+    log_dataset(dataset)
+
     
 def try_hiertext(base_folder = DEFAULT_HIERTEXT, split: ['train', 'val'] = 'train',
                  handwritten = [True, False], legibility = [True, False], mode = 'words',
@@ -55,16 +49,15 @@ def try_hiertext(base_folder = DEFAULT_HIERTEXT, split: ['train', 'val'] = 'trai
     dataset = HierTextDataset(base_folder, split,
                  handwritten, legibility , mode ,
                  image_height ,patch_width , transforms)
-    
-    print('total:', len(dataset))
-    out = dataset[IDX]
-    print(out)
-    out['original_image'].save(os.path.join(OUTPUT_TMP_FOLDER, f"{out['dataset']}_{out['split']}.png"))
+    log_dataset(dataset)
+
 
 def try_maps(base_folder = DEFAULT_HIST_MAPS, split: ["train", "test"] = 'train', cross_val = 'cv1', image_height = 128, patch_width = 16, transforms = lambda x: x):
     dataset =  HistoricalMapsdDataset(base_folder, split, cross_val, image_height, patch_width, transforms)
+    log_dataset(dataset)
 
-    print('total:', len(dataset))
-    out = dataset[IDX]
-    print(out)
-    out['original_image'].save(os.path.join(OUTPUT_TMP_FOLDER, f"{out['dataset']}_{out['split']}.png"))
+
+def try_iam(base_folder = DEFAULT_IAM, split: ["train", "test", "val"] = 'train', partition: ["aachen", "original"] = 'aachen', mode: ["words", "lines"] = "words", image_height = 128, patch_width = 16, transforms = lambda x: x):
+    dataset = IAMDataset(base_folder, split, partition, mode, image_height, patch_width, transforms)
+    log_dataset(dataset)
+
