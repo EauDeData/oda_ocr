8d1386e6b745323d89f3490152687440eddf40b4
EauDeData <evabot505@gmail.com>
2023-10-10 13:22:14 +0000

    text ocr implemented

--- dataset_tests.py
+++ dataset_tests.py
@@ -1,5 +1,5 @@
 from src.datasets.ocr.test.ocr_datasets_unitary_testing import (log_dataset, try_esposalles, try_cocotext, try_funsd, try_washinton, try_hiertext, try_maps, try_iam,\
-                                                                try_iii, try_mlt19, try_parzival, try_xfund, try_totaltext)
+                                                                try_iii, try_mlt19, try_parzival, try_xfund, try_totaltext, try_textocr)
 
 # try_esposalles()
 # try_cocotext()
@@ -12,6 +12,7 @@ from src.datasets.ocr.test.ocr_datasets_unitary_testing import (log_dataset, try
 # try_mlt19()
 # try_parzival()
 # try_xfund()
-try_totaltext()
+# try_totaltext()
+try_textocr()
 
-log_dataset(try_totaltext() + try_funsd() + try_iam(), idx = len(try_totaltext()) + 10)
+# log_dataset(try_totaltext() + try_funsd() + try_iam(), idx = len(try_totaltext()) + 10)
