090136a0999e46198f75e2ef9be65dc73f18f207
amolina <evabot505@gmail.com>
2023-11-14 16:00:28 +0100

    tocluster

--- src/vision/models.py
+++ src/vision/models.py
@@ -261,9 +261,6 @@ class TransformerDecoder(nn.Module):
 
         encoder_output = self.encoder(X)['features']  # Pass the batch X through the encoder
 
-        import pdb
-        pdb.set_trace()
-
         memory = self.memory(encoder_output)
 
         projected = self.gelu_fn(self.projection(encoder_output))  # Project encoder output to decoder token size
@@ -276,14 +273,9 @@ class TransformerDecoder(nn.Module):
             tgt_mask=tgt_mask
         ))
 
-        import pdb
-        pdb.set_trace()
 
         # Project the decoder output to vocabulary space
         output = self.lm_head(decoded)
-
-        import pdb
-        pdb.set_trace()
 
         return {
             'features': decoded,
